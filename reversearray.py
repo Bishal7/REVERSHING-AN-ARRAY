c9b821fbf7db5be9d47e8c892ae0c846f792bf47
Bishal7 <56946903+Bishal7@users.noreply.github.com>
2019-11-27 20:18:25 -0800

    PRGRAM TO REVERSE AN ARRAY

---
+++ reversearray.py
@@ -0,0 +1,11 @@
+# print("IN THIS PROGRAM WE ARE REVERSHING AN ARRAY")
+# arr = [0,20,24]
+# for i in range(len(arr)-1,-1,-1):
+#     print(arr[i])
+# print("COMPLETING REVERSE ")
+print("IN THIS PROGRAM WE ARE REVERSHING AN ARRAY")
+arr = input("please input the value to reverse:-")
+
+for i in range(len(arr)-1,-1,-1):
+    print(arr[i])
+print("COMPLETING REVERSE ")
